11366c3c133016bae6fb5041b35b4eebc57c7c68
hongjourney <carrotjy16@gmail.com>
2022-01-17 16:34:41 +0900

    bfs, dfs

--- Baekjoon/11116 교통량.py
+++ Baekjoon/11116 교통량.py
@@ -1,13 +1,36 @@
+"""
+일정 간격으로 떨어져있는 두 줄
+예를 들어 자동차가 왼쪽에서 오른쪽으로 갈 때, 두 줄을 지나면서
+[왼쪽 줄 시작, 왼쪽 줄 끝, 오른쪽 줄 시작, 오른쪽 줄 끝] = [t, t+500, t+1000, t+1500]
+와 같은 기록을 남긴다. (500간격)
+
+입력조건
+- 첫째줄 : 테스트 케이스 개수, n(1<=n<=100)
+- 각각의 테스트 케이스에 대해 
+    - 첫째줄 : 측정된 시간 기록 개수, m(m<=200)
+    - 둘째줄, 셋째줄 : 차례대로 왼쪽 기록 m개, 오른쪽 기록 m개 (시간기록은 10^9이하)
+- ex)  
+2
+4
+17 517 1432 1932
+432 932 1017 1517
+6
+235 451 735 951 2351 2851
+1235 1351 1451 1735 1851 1951
+
+출력조건
+- 왼쪽에서 오는 차의 숫자
+- ex)
+1
+2
+
+"""
 n = int(input()) #테스트 케이스 개수
 m = []
-left_list = []
-right_list = []
-
+left = []
+right = []
 for i in range(n):
     m.append(int(input()))
-    left = list(map(int, input().split()))
-    right = list(map(int, input().split()))
-    left_list.append(left)
-    right_list.append(right)
+    left.extend(list(map(int, input().split())))
+    right.extend(list(map(int, input().split())))
 
-for i in range(m-2)
---
+++ 이코테/BFS,DFS/특정거리의도시찾기.py
@@ -0,0 +1,53 @@
+
+"""
+입력 조건
+- 도시 개수 (2<=n<=300,000), 도로 개수 (1<=m<=1,000,000), 거리 정보 (1<=k<=300,000), 출발 도시 정보 (1<=x<=n)
+- (a,b): a번 도시에서 b번 도시로 이동하는 단방향 도로 존재. (1<=a,b<=n)
+- 거리는 1, x번-> x번 거리는 0
+
+출력 조건
+- x로부터 출발하여 도달할 수 있는 도시 중, 최단 거리가 k인 모든 도시의 번호를 오름차순으로 출력
+- 존재하지 않는다면 -1 출력
+    
+"""
+n, m, k, x = map(int,input().split())
+graph = [[] for _ in range(n+1)] # 인접 노드 리스트
+visited = [0]*(n+1) # 노드별 방문 여부
+distance = [0]*(n+1)
+
+for _ in range(m):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+
+from collections import deque
+
+def bfs(start, visited, graph, distance):
+    visited[start] = True
+    q = deque()
+    q.append(start)
+    while q:
+        x = q.popleft()
+        for i in graph[x]:
+            if not visited[i]:
+                q.append(i)
+                distance[i] = distance[x] + 1
+                visited[i] = True
+
+    return distance
+
+distance = bfs(x, visited, graph, distance)
+
+answer = [i for i, number in enumerate(distance) if number==k]
+
+if not answer:
+    print(-1)
+else:
+    print(sorted(answer))
+
+
+"""
+개선사항
+- visited 대신 distance = [-1]*(n+1)로 하고 distance[i]가 -1인지 확인해서 방문 여부 확인 가능
+- 번호가 이미 오름차순이므로 sorted 안 해도 됨
+- x와 i를 now, next_node 등으로 변수명 바꾸는게 이해하기 좋을듯
+"""
