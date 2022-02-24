6cc9fe4a9e81a5e28dfc4b07889f826b5c95debc
hongjourney <carrotjy16@gmail.com>
2022-02-24 22:34:02 +0900

    39-화성탐사 정리

--- 이코테/최단경로/39-화성탐사.py
+++ 이코테/최단경로/39-화성탐사.py
@@ -1,3 +1,15 @@
+"""
+입력 조건
+- 첫째 줄에 테스트 케이스 수 t, (1<=t<=10)
+- 매 테스트 케이스의 첫째 줄에는 그래프 사이즈인 정수 n이 주어짐. (2<=n<=125)
+- 공간은 n*n 크기의 2차원 공간
+- 이어서 n개의 줄에 걸쳐 각 칸의 비용이 주어짐. (0<=각 칸의 비용<=9)
+
+출력 조건
+- [0][0]위치에서 출발하여 [n-1][n-1] 위치까지 상하좌우로 이동
+- 최소 비용으로 이동한 결과를 출력
+"""
+
 test_count = int(input())
 import heapq
 
@@ -5,10 +17,11 @@ for _ in range(test_count):
     n = int(input())
     INF = int(1e9)
     graph = []
-    distance = [[INF]*n for _ in range(n)]
 
     for _ in range(n):
         graph.append(list(map(int, input().split())))
+
+    distance = [[INF]*n for _ in range(n)]
     
     def dijkstra(start_x, start_y):
         q = []
